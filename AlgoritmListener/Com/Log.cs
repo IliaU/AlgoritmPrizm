250cabc879d5e7143596e2947d89b3ccfc8616dc
IliaU <ilia82@mail.ru>
2023-06-03 14:27:18 +0300

    Добавил логирование в службе

---
+++ AlgoritmListener/Com/Log.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Threading;
+using AlgoritmListener.Lib;
+
+namespace AlgoritmListener.Com
+{
+    public static class Log
+    {
+        #region Private Param
+        /// <summary>
+        /// Количество попыток записи в лог
+        /// </summary>
+        private static int IOCountPoput = 5;
+
+        /// <summary>
+        /// Количество милесекунд мкжду попутками записи
+        /// </summary>
+        private static int IOWhileInt = 500;
+        #endregion
+
+        #region Public Param
+        /// <summary>
+        /// Файл в который будем сохранять лог
+        /// </summary>
+        public static string File { get; private set; }
+
+        /// <summary>
+        /// Директория
+        /// </summary>
+        public static string Dir { get; private set; }
+
+        /// <summary>
+        /// Возникновение события в приложении
+        /// </summary>
+        public static event EventHandler<EventLog> onEventLog;
+        #endregion
+
+
+        #region Public Method
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="LogDir">Путь для файла с логами</param>
+        /// <param name="FileLog">Имя файла для лога программы</param>
+        public static void SetupLog(string LogDir, string LogFile)
+        {
+            if (string.IsNullOrWhiteSpace(LogFile)) File = "AlgoritmListener.log";
+            else File = LogFile;
+
+            if (string.IsNullOrWhiteSpace(LogDir)) Dir = "";
+            else Dir = LogDir;
+
+            EventSave("Запуск службы", "AlgoritmListener", EventEn.Message);
+        }
+        
+        
+        /// <summary>
+        /// Событие программы
+        /// </summary>
+        /// <param name="Message">Сообщение</param>
+        /// <param name="Source">Источник</param>
+        /// <param name="evn">Тип события</param>
+        public static void EventSave(string Message, string Source, EventEn evn)
+        {
+            EventSave(File, Message, Source, evn, IOCountPoput);
+        }
+        #endregion
+
+        #region Private Method
+
+        /// </summary>
+        /// <param name="FileName">Файл в котрый пишем лог</param>
+        /// <param name="Message">Сообщение</param>
+        /// <param name="Source">Источник</param>
+        /// <param name="evn">Тип события</param>
+        /// <param name="IOCountPoput">Количество попыток записи в лог</param>
+        private static void EventSave(string FileName, string Message, string Source, EventEn evn, int IOCountPoput)
+        {
+            try
+            {
+                string newFile = (string.IsNullOrWhiteSpace(FileName) ? File : FileName);
+
+                using (StreamWriter SwFileLog = new StreamWriter(Dir + @"\" + newFile, true))
+                {
+                    SwFileLog.WriteLine(DateTime.Now.ToString() + "\t" + evn.ToString() + "\t" + Source + "\t" + Message);
+                }
+                
+            }
+            catch (Exception)
+            {
+                if (IOCountPoput > 0)
+                {
+                    Thread.Sleep(IOWhileInt);
+                    EventSave(FileName, Message, Source, evn, IOCountPoput - 1);
+                }
+                else throw;
+            }
+        }
+        
+        #endregion
+    }
+}
--- AlgoritmListener/Service1.cs
+++ AlgoritmListener/Service1.cs
@@ -8,27 +8,61 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using AlgoritmListener.Lib;
+using AlgoritmListener.Com;
+
 namespace AlgoritmListener
 {
     public partial class Service1 : ServiceBase
     {
+        /// <summary>
+        /// Место хранения лога
+        /// </summary>
+        public static string LogDir = @"C:\Program Files";
+        public static string LogFile = "AlgoritmListener.txt";
+
         public Service1()
         {
             InitializeComponent();
         }
 
+        // Запуск службы
         protected override async void OnStart(string[] args)
         {
+            // Флаг пишет что сервис стартанул
+            bool flagStardet = false;
+
             // Запускаем службу чтобы работала бесконечно
-            while(true)
+            while (true)
             {
+                // Обработка ошибок чтобы сервис не свалился
+                try
+                {
+                    if (!Directory.Exists(string.Format(@"{0}\AlgoritmListener", LogDir))) Directory.CreateDirectory(string.Format(@"{0}\AlgoritmListener", LogDir));
+
+                    // Если не писали о том что служба стартанула пишем об этом в лог
+                    if (!flagStardet)
+                    {
+                        Com.Log.SetupLog(string.Format(@"{0}\AlgoritmListener", LogDir), LogFile);
+                        flagStardet = true;
+                    }
+                }
+                catch (Exception) { }
+
                 // Пауза между циклами
                 await Task.Delay(3000);
             }
         }
 
+        // Остановка службы
         protected override void OnStop()
         {
+            try
+            {
+                Com.Log.EventSave("Cлужба остановлена.", "AlgoritmListener", EventEn.Message);
+            }
+            catch (Exception){}
         }
     }
 }
