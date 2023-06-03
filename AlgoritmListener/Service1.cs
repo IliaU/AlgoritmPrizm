8b822803fb5d24201c3cbb5b4a4b0044f7f8c2db
IliaU <ilia82@mail.ru>
2023-06-03 11:09:38 +0300

    Сделал запуск основной программы только с правами администратора чтобы по другому не стартовала и сделал шаблон службы для того чтобы потом устроить управление процесса прослушивания наших сервисев

---
+++ AlgoritmListener/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmListener
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnStart(string[] args)
+        {
+            // Запускаем службу чтобы работала бесконечно
+            while(true)
+            {
+                // Пауза между циклами
+                await Task.Delay(3000);
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
